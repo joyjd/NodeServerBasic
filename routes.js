6bd52b64f34950a5f618e8ecbe3c77d126b7847c
joyjd <joydas1611@gmail.com>
2024-03-16 23:39:46 +0100

    first draft

---
+++ routes.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedbody = Buffer.concat(body).toString();
+            const message = parsedbody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+
+
+        });
+
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hello World</h1>');
+    res.end();
+}
+
+module.exports = requestHandler;
